781e5294f3d866d46fdead6dfb51bcc413bd6813
Nguyen Nam <justn4m@gmail.com>
2018-10-23 05:07:23 +0700

    change the home page

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Log;
-class LoginController extends Controller{
-   // public function FuncName(Request $request){
-   
-   //    // log something to storage/logs/laravel.log
-   //    Log::info(['Request'=>$request]);}
-   // }
-   public function FuncName(Request $request){
-      
-      // log something to storage/logs/debug.log
-      Log::useDailyFiles(storage_path().'/logs/debug.log');
-      Log::info(['Request'=>$request]);
-   }
+class LoginController extends Controller
+{
+    // public function FuncName(Request $request){
+
+    //    // log something to storage/logs/laravel.log
+    //    Log::info(['Request'=>$request]);}
+    // }
+    public function FuncName(Request $request)
+    {
+
+        // log something to storage/logs/debug.log
+        Log::useDailyFiles(storage_path() . '/logs/debug.log');
+        Log::info(['Request' => $request]);
+    }
+}
 ?>
