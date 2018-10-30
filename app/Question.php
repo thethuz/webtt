b7b8a104ff09ef8cd68f19f63a98aed62b0d8496
Nguyen Nam <justn4m@gmail.com>
2018-10-30 17:12:07 +0700

    check update

--- app/Question.php
+++ app/Question.php
@@ -9,4 +9,14 @@ class Question extends Model
     protected $table = "questions";
 
     public $timestamps = true;
+
+    public function user()
+    {
+        return $this->hasOne('App/User', 'id', 'created_by');
+    }
+
+    public function tag()
+    {
+        return $this->hasMany('App/Tag');
+    }
 }
--- app/UserVote.php
+++ app/UserVote.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserVote extends Model
 {
-    //
+    protected $table = 'user_vote';
+    public $timestamps = false;
 }
