e9d9ff2d90505504a48aa4ece2ca55d21012c9ac
thethuz1 <nthethu@gmail.com>
2018-11-18 21:57:07 +0700

    add tags controller

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Tag;
+use App\Question;
+
+use DB;
+
 class TagsController extends Controller
 {
     /**
@@ -11,10 +15,16 @@ class TagsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
-    }
+     protected function getTopTags() {
+
+       $tags = DB::table('tags')
+       ->select('name', DB::raw('count(question_id) as total'))
+       ->groupBy('name')
+       ->orderBy('total', 'desc')
+       ->paginate(10);
+
+       return $tags;
+     }
 
     /**
      * Show the form for creating a new resource.
@@ -35,6 +45,26 @@ class TagsController extends Controller
     public function store(Request $request)
     {
         //
+        try {
+            if (!$request->body or !$request->user_id) {
+                $result = [
+                    'message' => 'Please Provide Both body and user_id',
+                    'data' => []
+                ];
+                return response()->json(['data' => $result], \Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+            Tag::create($request->all());
+            $result = [
+                'message' => 'Post Created Succesfully',
+            ];
+            return response()->json(['data' => $result], \Illuminate\Http\Response::HTTP_OK);
+        } catch (Exception $e) {
+            $result = [
+                'message' => $e->getMessage(),
+                'data' => []
+            ];
+            return response()->json(['data' => $result], \Illuminate\Http\Response::HTTP_BAD_REQUEST);
+        }
     }
 
     /**
@@ -43,10 +73,30 @@ class TagsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
-    }
+     public function showQuestionByTag($tag) {
+
+       $questions = Question::
+                   whereHas('tag', function ($query) use ($tag) {
+                       $query->where('name', '=', $tag);
+                   })
+                   ->orderBy('created_at', 'desc')->paginate(10);
+
+       $newest_questions = Question::orderBy('created_at', 'desc')->paginate(20);
+
+       $question_count = Question::count();
+
+       foreach ($questions as $key => $question) {
+         $questions[$key]->votes = $this->countUpVoteByVoteId($question->id, Config::get('constants.VOTE_CATEGORY.QUESTION') )
+                            - $this->countDownVoteByVoteId($question->id, Config::get('constants.VOTE_CATEGORY.QUESTION') );
+
+         $questions[$key]->answers = $this->countAnswerByQuestionId($question->id);
+       }
+
+       $top_tags = $this->getTopTags();
+
+       return view('question.list', ['questions' => $questions, 'newest_questions' => $newest_questions
+                                     ,'question_count' => $question_count, 'top_tags' => $top_tags ]);
+     }
 
     /**
      * Show the form for editing the specified resource.
@@ -69,6 +119,29 @@ class TagsController extends Controller
     public function update(Request $request, $id)
     {
         //
+        try {
+            if (!$request->body or !$request->user_id) {
+                $result = [
+                    'message' => 'Please Provide Both body and user_id',
+                    'data' => []
+                ];
+                return response()->json(['data' => $result], \Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+            $post = Posts::find($id);
+            $post->body = $request->body;
+            $post->user_id = $request->user_id;
+            $post->save();
+            $result = [
+                'message' => 'Post Updated Succesfully'
+            ];
+            return response()->json(['data' => $result], \Illuminate\Http\Response::HTTP_OK);
+        } catch (Exception $e) {
+            $result = [
+                'message' => $e->getMessage(),
+                'data' => []
+            ];
+            return response()->json(['data' => $result], \Illuminate\Http\Response::HTTP_BAD_REQUEST);
+        }
     }
 
     /**
@@ -80,5 +153,18 @@ class TagsController extends Controller
     public function destroy($id)
     {
         //
+        try {
+            Tag::destroy($id);
+            $result = [
+                'message' => 'Post Deleted Succesfully'
+            ];
+            return response()->json(['data' => $result], \Illuminate\Http\Response::HTTP_OK);
+        } catch (Exception $e) {
+            $result = [
+                'message' => $e->getMessage(),
+                'data' => []
+            ];
+            return response()->json(['data' => $result], \Illuminate\Http\Response::HTTP_BAD_REQUEST);
+        }
     }
 }
